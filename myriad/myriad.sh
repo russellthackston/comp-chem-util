e279371042e1adce6a2fe9e9fd38e9919c5428b3
Russell Thackston <rthackston@ITEC2552108X001.local>
2016-03-10 14:35:05 -0500

    Streamlined code and removed dead sections.

--- myriad/myriad.sh
+++ myriad/myriad.sh
@@ -1,5 +1,56 @@
+function uploadFiles() {
+        echo Uploading output files...
+        UUID=$(uuidgen)
+        echo Using common ID ${UUID}
+
+        for f in *
+        do
+                OUTPUT_REQUEST="${BASE_URL}"'/output/'"${JOB_NUMBER}"'?machineID='"${MAC_ID}"'&filename='"${f}"'&uuid='"${UUID}"
+                if [ -s $f ]; then
+                        echo Uploading $f
+                        echo Accessing web service: $OUTPUT_REQUEST
+                        curl --request POST ${OUTPUT_REQUEST} -H "Content-Type: text/plain" --data-binary "@${f}"
+                else
+                        echo Skipping upload of empty file $f
+                fi
+        done
+}
+
+function startJob() {
+        rm -Rf $SCRATCH/* 2> /dev/null
+
+        # Make a folder for this job
+        DIR_NAME=$(date +%Y%m%d_%H%M%S)
+        mkdir $DIR_NAME
+        pushd $DIR_NAME
+
+        # Get the next job in the jobset
+        echo Getting the next job in the jobset
+        if [[ "$JOB_TYPE" == "bench" ]]; then
+                INPUT_REQUEST="${BASE_URL}"'/benchmarking/'"${BENCHMARK_JOBSET_NUMBER}"'?machineID='"${MAC_ID}"
+        fi
+        if [[ "$JOB_TYPE" == "set" ]]; then
+                INPUT_REQUEST="${BASE_URL}"'/jobrunner/'"${JOBSET_NUMBER}"'?machineID='"${MAC_ID}"
+        fi
+
+        echo Accessing web service: $INPUT_REQUEST
+        curl --request GET ${INPUT_REQUEST} -w "\n\n# Response Code: %{http_code}\n" -d "" > input.dat
+        echo Input.dat file written to $(pwd)
+
+        RESP_CODE=$(tail -n 1 input.dat | cut -d':' -f 2)
+        RESP_CODE="$(echo -e "${RESP_CODE}" | tr -d '[[:space:]]')"
+        echo Identified response code as $RESP_CODE
+
+	popd
+
+        if [ "$RESP_CODE" != "200" ]; then
+                echo No more jobs found in job set
+                rm -Rf $DIR_NAME
+        fi
+}
+
 # Check for command line parameters
-USAGE="USAGE: myriad.sh -u BASE_URL -m MACHINE_NAME (-b BENCHMARK_JOBSET_NUMBER | -j JOBSET_NUMBER) [-h] [-l (directory)]"
+USAGE="USAGE: myriad.sh -u BASE_URL -m MACHINE_NAME (-b BENCHMARK_JOBSET_NUMBER | -j JOBSET_NUMBER) [-h] [-l]\nThe -l flag only uploads existing files in the current folder. No jobs are run."
 SHOW_USAGE="false"
 BASE_URL="unknown"
 MACHINE_NAME="unknown"
@@ -8,6 +59,7 @@ JOBSET_NUMBER="unknown"
 JOB_TYPE="unknown"
 UPLOAD_FILES="unknown"
 UPLOAD_FROM="unknown"
+ETH_ADAPTER_NAME="eth0"
 
 while [[ $# > 0 ]]
 do
@@ -31,13 +83,15 @@ do
 		JOB_TYPE="set"
 		shift
 		;;
+		-e)
+		ETH_ADAPTER_NAME="$2"
+		shift
+		;;
 		-h)
 		SHOW_USAGE="true"
 		;;
 		-l)
 		UPLOAD_FILES="true"
-		UPLOAD_FROM=$2
-		shift
 		;;
 	esac
 	shift
@@ -45,16 +99,6 @@ done
 
 if [[ "$SHOW_USAGE" == "true" ]]; then
 	echo $USAGE
-	exit 0
-fi
-
-if [[ "$UPLOAD_FILES" == "true" ]]; then
-	if [[ "$UPLOAD_FROM" == "unknown" ]]; then
-		echo "Missing directory name to upload from"
-		echo $USAGE
-		exit 1
-	fi
-	uploadFiles($UPLOAD_FROM);
 	exit 0
 fi
 
@@ -76,139 +120,19 @@ if [[ "$JOB_TYPE" == "unknown" ]]; then
         exit 1
 fi
 
-# Write parameters to their respective files
-if [[ "$BASE_URL" != "unknown" ]]; then
-	echo $BASE_URL > url.txt
-fi
-if [[ "$MACHINE_NAME" != "unknown" ]]; then
-	echo $MACHINE_NAME > name.txt
-fi
-if [[ "$BENCHMARK_JOBSET_NUMBER" != "unknown" ]]; then
-	echo $BENCHMARK_JOBSET_NUMBER > bench.txt
-fi
-if [[ "$JOBSET_NUMBER" != "unknown" ]]; then
-	echo $JOBSET_NUMBER > jobset.txt
+if [[ "$UPLOAD_FILES" == "true" ]]; then
+        uploadFiles
+        exit 0
 fi
 
 # http://myriad.elasticbeanstalk.com/api
 SCRATCH=/tmp
 
-function uploadFiles($ff) {
-	pushd $ff
-
-	echo Uploading output files...
-        UUID=$(uuidgen)
-        echo Using common ID ${UUID}
-
-	for f in *
-        do
-                OUTPUT_REQUEST="${BASE_URL}"'/output/'"${JOB_NUMBER}"'?machineID='"${MAC_ID}"'&filename='"${f}"'&uuid='"${UUID}"
-                if [ -s $f ]; then
-                        echo Uploading $f
-                        echo Accessing web service: $OUTPUT_REQUEST
-                        curl --request POST ${OUTPUT_REQUEST} -H "Content-Type: text/plain" --data-binary "@${f}"
-                else
-                        echo Skipping upload of empty file $f
-                fi
-        done
-
-	popd
-}
-
-function startJob() {
-	rm -Rf $SCRATCH/* 2> /dev/null
-
-	# Make a folder for this job
-	DIR_NAME=$(date +%Y%m%d_%H%M%S)
-	mkdir $DIR_NAME
-	pushd $DIR_NAME
-
-	# Get the next job in the jobset
-	echo Getting the next job in the jobset
-	if [[ "$JOB_TYPE" == "bench" ]]; then
-		INPUT_REQUEST="${BASE_URL}"'/benchmarking/'"${BENCHMARK_JOBSET_NUMBER}"'?machineID='"${MAC_ID}"
-	fi
-	if [[ "$JOB_TYPE" == "set" ]]; then
-                INPUT_REQUEST="${BASE_URL}"'/jobrunner/'"${JOBSET_NUMBER}"'?machineID='"${MAC_ID}"
-        fi
-
-	echo Accessing web service: $INPUT_REQUEST
-	curl --request GET ${INPUT_REQUEST} -w "\n\n# Response Code: %{http_code}\n" -d "" > input.dat
-	echo Input.dat file written to $(pwd)
-
-	RESP_CODE=$(tail -n 1 input.dat | cut -d':' -f 2)
-	RESP_CODE="$(echo -e "${RESP_CODE}" | tr -d '[[:space:]]')"
-	echo Identified response code as $RESP_CODE
-
-	if [ "$RESP_CODE" != "200" ]; then
-	        echo No more jobs found in job set
-	        popd
-	        rm -Rf $DIR_NAME
-	fi
-}
-
-# Check for a previous URL
-if [[ -f url.txt ]]; then
-        BASE_URL=$(cat url.txt)
-fi
-
-# If the file contained a non-blank url, use it
-if [[ "$BASE_URL" != "" ]]; then
-        echo Url $BASE_URL read from url.txt
-else
-# If there was not a url in the file, get one from the user
-        BASE_URL=Unknown
-        nm1=foo
-        nm2=bar
-        while [ "$nm1" != "$nm2" ]
-        do
-                echo -n "Enter the url of the web service and press [ENTER]: "
-                read nm1
-                echo -n "Reenter the url of the web service and press [ENTER]: "
-                read nm2
-                if [ "$nm1" != "$nm2" ]; then
-                        echo "Urls do not match"
-                else
-                        BASE_URL=$nm1
-                fi
-        done
-        echo Url set to $BASE_URL
-        echo $BASE_URL > url.txt
-fi
-
-# Check for a previous machine name
-if [[ -f name.txt ]]; then
-	MAC_NAME=$(cat name.txt)
-fi
-
-# If the file contained a non-blank name, use it
-if [[ "$MAC_NAME" != "" ]]; then
-	echo Machine name $MAC_NAME read from name.txt
-else
-# If there was not a name in the file, get one from the user
-	MAC_NAME=Unknown
-	nm1=foo
-	nm2=bar
-	while [ "$nm1" != "$nm2" ]
-	do
-	        echo -n "Enter this machine's name and press [ENTER]: "
-        	read nm1
-	        echo -n "Reenter this machine's name and press [ENTER]: "
-        	read nm2
-        	if [ "$nm1" != "$nm2" ]; then
-                	echo "Names do not match"
-        	else
-                	MAC_NAME=$nm1
-        	fi
-	done
-	echo Machine name set to $MAC_NAME
-	echo $MAC_NAME > name.txt
-fi
-
 # Get the MAC address and store in a file for later
-MAC_ADDRESS=$(ifconfig en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
-if [ "$MAC_ADDRESS" = "" ]; then
-	MAC_ADDRESS=$(ifconfig eth0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+MAC_ADDRESS="$(ifconfig "${ETH_ADAPTER_NAME}" | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')"
+if [[ "$MAC_ADDRESS" == "" ]]; then
+	echo "Could not get a MAC address. Try adding the -e (adapter name) flag to the command."
+	exit 1
 fi
 
 # Replace colons in MAC address and write to file
@@ -217,42 +141,13 @@ echo $MAC_ADDRESS > mac.txt
 echo MAC address determined to be $MAC_ADDRESS
 
 # Build the URL to request a new/existing machine ID
-POST_REQUEST="${BASE_URL}"'/machines/'"${MAC_ADDRESS}"'?name='"${MAC_NAME}"
+POST_REQUEST="${BASE_URL}"'/machines/'"${MAC_ADDRESS}"'?name='"${MACHINE_NAME}"
 echo Accessing web service: $POST_REQUEST
 
 # Call the web service and store the response value
 MAC_ID=$(curl --request POST ${POST_REQUEST} -d "")
 echo $MAC_ID > id.txt
 echo Your machine ID is $MAC_ID
-
-#If a jobset file exists, read its contents
-BENCHMARK_JOBSET_NUMBER=""
-if [[ -f bench.txt ]]; then
-	BENCHMARK_JOBSET_NUMBER=$(cat bench.txt)
-	if [ "$BENCHMARK_JOBSET_NUMBER" != "" ]; then
-		JOB_TYPE="bench"
-	fi
-fi
-if [[ -f jobset.txt ]]; then
-        JOBSET_NUMBER=$(cat jobset.txt)
-	if [ "$JOBSET_NUMBER" != "" ]; then
-		JOB_TYPE="set"
-	fi
-fi
-
-# If a jobset number was found in the file, use it
-case "$JOB_TYPE" in
-	bench)
-	echo "Jobset number $BENCHMARK_JOBSET_NUMBER found in bench.txt"
-        ;;
-	set)
-	echo "Jobset number $JOBSET_NUMBER found in jobset.txt"
-	;;
-	*)
-	echo "Missing benchmarking or jobset number"
-	exit 0
-	;;
-esac
 
 startJob
 
@@ -263,7 +158,7 @@ while [ "$RESP_CODE" = "200" ]; do
 	JOB_NUMBER="$(echo -e "${JOB_NUMBER}" | tr -d '[[:space:]]')"
 	echo Identified job number as $JOB_NUMBER
 
-	if [[ "$OSTYPE" = "linux" || "$OSTYPE" = "linux-gnu" ]]; then
+	if [[ $OSTYPE == *"linux"* ]]; then
 		CORES=$(lscpu|grep 'CPU(s)'|head -n 1|tr -s ' '|cut -f 2 -d ' ')
 	else
 		CORES=$(sysctl hw.ncpu | cut -f 2 -d ' ')
@@ -271,7 +166,7 @@ while [ "$RESP_CODE" = "200" ]; do
 	echo $CORES > cpu.txt
 
 	# Check the free memory
-	if [[ "$OSTYPE" = "linux" || "$OSTYPE" = "linux-gnu" ]]; then
+	if [[ $OSTYPE == *"linux"* ]]; then
 		FREE_MEM=$(free -m | grep Mem: | tr -s ' ' | cut -f 4 -d ' ')
 	else
 		FREE_MEM=$(top -l 1 -s 0 | grep 'PhysMem' | cut -f 6 -d ' ')
@@ -282,7 +177,7 @@ while [ "$RESP_CODE" = "200" ]; do
 	sed -i -e "s/memory .*/memory ${FREE_MEM} MB/" input.dat
 
 	echo Setting up to run PSI4 job...
-	if [[ "$OSTYPE" = "linux" || "$OSTYPE" = "linux-gnu" ]]; then
+	if [[ $OSTYPE == *"linux"* ]]; then
 		export OMP_NUM_THREADS=$CORES
                 export MKL_NUM_THREADS=$CORES
 	else
@@ -292,7 +187,7 @@ while [ "$RESP_CODE" = "200" ]; do
 	echo Set cores to $CORES with $FREE_MEM MB per core
 
 	echo Running PSI4 job...
-	if [[ "$OSTYPE" = "linux" || "$OSTYPE" = "linux-gnu" ]]; then
+	if [[ $OSTYPE == *"linux"* ]]; then
 		/usr/bin/time -v -o "time.out" psi4 > psi4.out 2> psi4.err
 	else
 		/usr/bin/time psi4 > psi4.out 2> psi4.err
@@ -302,15 +197,13 @@ while [ "$RESP_CODE" = "200" ]; do
 	# Record the current disk usage
 	echo Recording the current disk space and disk usage
 	df -h > disk.txt
-	ls -alh $SCRATCH >> disk.txt
+	ls -alh $SCRATCH >> scratch.txt
 
-	uploadFiles(".");
+	uploadFiles
 
 	# Clear the scratch space
 	echo clearing the scratch folder
 	rm -Rf $SCRATCH/* 2> /dev/null
-
-	popd
 
 	startJob
 
