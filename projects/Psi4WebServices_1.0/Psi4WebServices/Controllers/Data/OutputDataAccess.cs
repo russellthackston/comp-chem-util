94bc265b0331ffae7f871ca8cadd5223778bc2c2
Russell Thackston <Russell Thackston>
2016-03-11 15:24:24 -0500

    Added info to main webpage.

--- projects/Psi4WebServices_1.0/Psi4WebServices/Controllers/Data/OutputDataAccess.cs
+++ projects/Psi4WebServices_1.0/Psi4WebServices/Controllers/Data/OutputDataAccess.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Web;
 
 using MySql.Data.MySqlClient;
+using Psi4WebServices.Models;
 
 namespace Psi4WebServices.Controllers.Data
 {
     public class OutputDataAccess : BaseDataAccess
     {
+        public long GetCount()
+        {
+            MySqlCommand cmd = this.getCommand("SELECT COUNT(*) FROM JobResultsSummary");
+            long count = (long)cmd.ExecuteScalar();
+            return count;
+        }
+
+        public long GetCountSuccess()
+        {
+            MySqlCommand cmd = this.getCommand("SELECT COUNT(*) FROM JobResultsSummary WHERE Success = 1");
+            long count = (long)cmd.ExecuteScalar();
+            return count;
+        }
+
         public void Add(int id, string fileContents, int machineID, string filename, string uuid)
         {
             MySqlCommand cmd = this.getCommand("INSERT INTO JobResults (JobID, OutputFile, ResultsCollected, MachineID, Filename, JobGUID) VALUES (@id, @output, NOW(), @machine, @filename, @uuid)");
