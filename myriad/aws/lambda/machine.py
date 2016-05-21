33a20c0b4b16ef299ec5fd6db8082812cee6a9d6
Russell Thackston <rthackston@georgiasouthern.edu>
2016-05-21 00:45:21 -0400

    Stripped web service to bare minimum

--- myriad/aws/lambda/machine.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class Machine:
-    
-    def __init__(self, id="", name="", mac="", created=""):
-        self.id = id
-        self.name = name
-        self.mac = mac
-        self.created = created
