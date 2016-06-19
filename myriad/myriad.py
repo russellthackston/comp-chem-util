dd871a111b6f5b847ab54564c7a8a354fb572122
Russell Thackston <rthackston@georgiasouthern.edu>
2016-06-18 22:45:02 -0400

    Expanding stub for reference implementation of Myriad in Python.

---
+++ myriad/myriad.py
@@ -0,0 +1,18 @@
+# Function: Load configuration from config.txt
+
+# Function: Upload results to web service
+
+# Function: Start psi4 job
+
+# Main
+# loadConfig
+# startJob
+# Loop:
+#   Get system specs
+#   Configure psi4
+#   Run psi4
+#   Check exit code
+#   Upload results
+#   Clear scratch
+#   Start new job
+
