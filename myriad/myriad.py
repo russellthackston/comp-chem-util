55aae73ddff276a9d68217a07957ca9d98944764
Russell Thackston <rthackston@georgiasouthern.edu>
2016-06-20 10:48:58 -0400

    Expanding stub for reference implementation of Myriad in Python (2).

--- myriad/myriad.py
+++ myriad/myriad.py
@@ -42,7 +42,7 @@ class Myriad:
                         # Check for logical error in response
                         if not "errorMessage" in r.text:
                                 print("Good response:\n" + str(r.text))
-                                return self.parseJob(r.text)
+                                return self.parseJob(r.text.split('\n'))
                         else:
                                 # logic error
                                 print("Error from web service:\n" + str(r.text))
