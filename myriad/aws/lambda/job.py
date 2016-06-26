92a7bb0dfc5a05c3634f5d48bf94c8b2162402d9
Russell Thackston <rthackston@georgiasouthern.edu>
2016-06-26 16:12:17 -0400

    Removed obsolete files.

--- myriad/aws/lambda/job.py
+++
@@ -1,9 +0,0 @@
-class Job:
-    
-    def __init__(self, id="", name="", jobDefinition="", created="", makeInputDatParameters="", jobGroup=""):
-        self.id = id
-        self.name = name
-        self.jobDefinition = jobDefinition
-        self.created = created
-        self.makeInputDatParameters = makeInputDatParameters
-        self.jobGroup = jobGroup
