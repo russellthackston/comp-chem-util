2d59cd628cf96aff8e50c2eb6310240012b21f00
Russell Thackston <rthackston@georgiasouthern.edu>
2016-10-24 14:14:51 -0400

    Added logging to replace print()

--- myriad/bootstrap-myriad.py
+++ myriad/bootstrap-myriad.py
@@ -2,6 +2,7 @@ import argparse
 import glob
 import importlib
 import libmyriad
+import logging
 import myriad
 import os
 import requests
@@ -12,68 +13,72 @@ import time
 
 class Bootstrap:
 
-        server = 'https://raw.githubusercontent.com/russellthackston/comp-chem-util/master/myriad/'
-        
-        def __init__(self):
-                pass
+	self.server = 'https://raw.githubusercontent.com/russellthackston/comp-chem-util/master/myriad/'
+	self.version = '1.0'
 
-        def downloadMyriad(self):
-                self.downloadMyriadFile('libmyriad.py')
-                self.downloadMyriadFile('myriad.py')
-                importlib.reload(libmyriad)
-                importlib.reload(myriad)
+	def __init__(self):
+		pass
 
-        def downloadMyriadFile(self, filename):
-                r = requests.get(self.server + filename)
-                f = open(filename, 'w')
-                f.write(r.text)
-                f.flush()
-                f.close()
+	def downloadMyriad(self):
+		self.downloadMyriadFile('libmyriad.py')
+		self.downloadMyriadFile('myriad.py')
+		importlib.reload(libmyriad)
+		importlib.reload(myriad)
 
-        def run(self, jobGroup=None, jobCategory=None):
-                print("Job group = " + str(jobGroup))
-                print("Job category = " + str(jobCategory))
-                result = libmyriad.ResultCode.success
-                while(True):
+	def downloadMyriadFile(self, filename):
+		r = requests.get(self.server + filename)
+		f = open(filename, 'w')
+		f.write(r.text)
+		f.flush()
+		f.close()
 
-                        # run a myriad job
-                        m = myriad.Myriad()
-                        result = m.runOnce(jobGroup, jobCategory)
+	def run(self, jobGroup=None, jobCategory=None):
+		print("Job group = " + str(jobGroup))
+		print("Job category = " + str(jobCategory))
+		result = libmyriad.ResultCode.success
+		while(True):
 
-                        # Upload job folder(s) to S3 and delete for local drive
-                        if glob.glob("*.zip"):
-                                command = "aws s3 cp *.zip s3://myriaddropbox/"
-                                process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-                                command = "rm -Rf *.zip"
-                                process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+			# run a myriad job
+			m = myriad.Myriad()
+			result = m.runOnce(jobGroup, jobCategory)
 
-                        if result == libmyriad.ResultCode.success:
-                                print('Job completed. Checking for another job.')
-                        elif result == libmyriad.ResultCode.failure:
-                                print('Job failed. Retrying in 10 seconds...')
-                                time.sleep(10)
-                        elif result == libmyriad.ResultCode.noaction:
-                                # this file should be created by the start-up script or manually by the user
-                                if os.path.isfile('shutdown.myriad'):
-                                        print('No jobs found. Shutting down...')
-                                        
-                                        command = "/sbin/shutdown -h now"
-                                        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-                                print('No jobs found. Retrying in 60 seconds...')
-                                time.sleep(60)
+			# Upload job folder(s) to S3 and delete for local drive
+			if glob.glob("*.zip"):
+				command = "aws s3 cp *.zip s3://myriaddropbox/"
+				process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+				command = "rm -Rf *.zip"
+				process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 
-                        if os.path.isfile('die.myriad'):
-                                return
-                        while os.path.isfile('pause.myriad'):
-                                time.sleep(5)
+			if result == libmyriad.ResultCode.success:
+				print('Job completed. Checking for another job.')
+			elif result == libmyriad.ResultCode.failure:
+				print('Job failed. Retrying in 10 seconds...')
+				time.sleep(10)
+			elif result == libmyriad.ResultCode.noaction:
+				# this file should be created by the start-up script or manually by the user
+				if os.path.isfile('shutdown.myriad'):
+					print('No jobs found. Shutting down...')
+					command = "/sbin/shutdown -h now"
+					process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+				print('No jobs found. Retrying in 60 seconds...')
+				time.sleep(60)
 
-                        # Download a (potentially) updated copy of Myriad
-                        self.downloadMyriad()
+			if os.path.isfile('die.myriad'):
+				return
+			while os.path.isfile('pause.myriad'):
+				time.sleep(5)
+
+			# Download a (potentially) updated copy of Myriad
+			self.downloadMyriad()
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--group', dest='group', action='store', type=str, help='Optional group for filtering the requested job')
 parser.add_argument('--subGroup', dest='subGroup', action='store', type=str, help='Optional sub group for filtering the requested job')
 args = parser.parse_args()
+
+logging.basicConfig(filename='myriad.log',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s')
+logging.info("Bootstrapping Myriad...")
 
 b = Bootstrap()
 b.run(args.group, args.subGroup)
