8f621cc41cb4c8f459093583f3d14dd067ae2fa4
Russell Thackston <rthackston@georgiasouthern.edu>
2016-11-13 11:11:21 -0500

    refactoring

--- myriad/examples/userdata.sh
+++ myriad/examples/userdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+echo "userdata.sh script launched..."
 mkdir /home/ec2-user/logs/
-echo "userdata.sh script launched..." >> logs/userdata.log 2>&1
-cd /home/ec2-user/ >> logs/userdata.log 2>&1
-curl -o ec2-user-script-myriad.sh https://s3.amazonaws.com/psi4share/ec2-user-script-myriad.sh >> logs/userdata.log 2>&1
-chmod +x ec2-user-script-myriad.sh >> logs/userdata.log 2>&1
-echo "Launching ec2-user-script-myriad.sh..." >> logs/userdata.log 2>&1
+cd /home/ec2-user/
+curl -o ec2-user-script-myriad.sh https://s3.amazonaws.com/psi4share/ec2-user-script-myriad.sh
+chmod +x ec2-user-script-myriad.sh
+echo "Launching ec2-user-script-myriad.sh..."
 ./ec2-user-script-myriad.sh
-echo "Finished ec2-user-script-myriad.sh" >> logs/userdata.log 2>&1
+echo "Finished ec2-user-script-myriad.sh"
