71b0f73eec8a030ff949c01f6410586558070005
Russell Thackston <rthackston@vl215-063.vl215.georgiasouthern.edu>
2016-11-16 13:47:16 -0500

    Major refactoring

--- myriad/bootstrap-myriad.py
+++ myriad/bootstrap-myriad.py
@@ -14,8 +14,8 @@ import time
 class Bootstrap:
 
 	def __init__(self):
-		self.server = 'https://raw.githubusercontent.com/russellthackston/comp-chem-util/master/myriad/'
-		self.version = '1.0'
+		self.server = 'https://raw.githubusercontent.com/russellthackston/comp-chem-util/master/myriad'
+		self.version = 'stable'
 
 	def downloadMyriad(self):
 		self.downloadMyriadFile('libmyriad.py')
@@ -24,56 +24,49 @@ class Bootstrap:
 		importlib.reload(myriad)
 
 	def downloadMyriadFile(self, filename):
-		r = requests.get(self.server + filename)
+		r = requests.get(self.server + "/" + self.version + "/" + filename)
 		f = open(filename, 'w')
 		f.write(r.text)
 		f.flush()
 		f.close()
 
-	def run(self, jobGroup=None, jobCategory=None):
+	def run(self, jobGroup=None, jobCategory=None, server='https://raw.githubusercontent.com/russellthackston/comp-chem-util/master/myriad/', version='stable'):
 		logging.info("Job group = " + str(jobGroup))
 		logging.info("Job category = " + str(jobCategory))
-		result = libmyriad.ResultCode.success
-		while(True):
+		logging.info("Server = " + str(server))
+		logging.info("Version = " + str(version))
 
-			# run a myriad job
-			m = myriad.Myriad()
-			result = m.runOnce(jobGroup, jobCategory)
+		# Download a (potentially) updated copy of Myriad
+		self.server = server
+		self.version = version
+		self.downloadMyriad()
 
-			if result == libmyriad.ResultCode.shutdown:
-				logging.info('Shutting down myriad...')
-			elif result == libmyriad.ResultCode.success:
-				# If success, upload job folder(s) to S3 and delete from local drive
-				logging.info('Job completed successfully.')
-			elif result == libmyriad.ResultCode.failure:
-				logging.info('Job failed. Retrying in 10 seconds...')
-				time.sleep(10)
-			elif result == libmyriad.ResultCode.noaction:
-				# this file should be created by the start-up script or manually by the user
-				# Or it may be created by Myriad if it detects a Spot Instance being terminated
-				if os.path.isfile('shutdown.myriad'):
-					logging.info('Shutdown requested...')
-					command = "/sbin/shutdown -h +1"
-					process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-				logging.info('No jobs found. Retrying in 60 seconds...')
-				time.sleep(60)
+		# run a myriad job
+		m = myriad.Myriad()
+		result = m.runOnce(jobGroup, jobCategory)
 
-			if os.path.isfile('die.myriad') or os.path.isfile('shutdown.myriad'):
-				return
-			while os.path.isfile('pause.myriad'):
-				time.sleep(5)
+		if result == libmyriad.ResultCode.shutdown or os.path.isfile('shutdown.myriad'):
+			logging.info('Shutting down myriad...')
+			return
+		elif result == libmyriad.ResultCode.failure:
+			logging.info('Job failed.')
+		elif result == libmyriad.ResultCode.noaction:
+			logging.info('No more jobs. Sleeping for 60 seconds...')
+			# TO DO: Move this sleep to ec2-user-script-myriad.sh (Needs return codes)
+			time.sleep(60)
 
-			# Download a (potentially) updated copy of Myriad
-			self.downloadMyriad()
+		# Download a (potentially) updated copy of Myriad
+		self.downloadMyriad()
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--group', dest='group', action='store', type=str, help='Optional group for filtering the requested job')
 parser.add_argument('--subGroup', dest='subGroup', action='store', type=str, help='Optional sub group for filtering the requested job')
+parser.add_argument('--server', dest='server', action='store', type=str, help='Optional server address for updating Myriad')
+parser.add_argument('--version', dest='version', action='store', type=str, help='Optional version number of Myriad to update to or "stable"')
 args = parser.parse_args()
 
-logging.basicConfig(filename='myriad.log',level=logging.INFO)
-logging.basicConfig(format='%(asctime)s %(message)s')
+logging.basicConfig(filename='logs/myriad.log',level=logging.INFO,format='%(asctime)s %(message)s')
 logging.info("Bootstrapping Myriad...")
 
 b = Bootstrap()
-b.run(args.group, args.subGroup)
+b.run(jobGroup=args.group, jobCategory=args.subGroup, server=args.server, version=args.version)
